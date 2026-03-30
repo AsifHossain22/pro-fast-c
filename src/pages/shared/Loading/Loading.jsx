f9e025c0766221067a66d55b40d6dfb0a4df76c8
AsifHossain22 <only1asif4you22@gmail.com>
2026-03-30 11:24:18 +0400

    Set-up Private Route

---
+++ src/pages/shared/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div>
+      <span className="loading loading-ring loading-xl bg-red-500"></span>
+    </div>
+  );
+};
+
+export default Loading;
